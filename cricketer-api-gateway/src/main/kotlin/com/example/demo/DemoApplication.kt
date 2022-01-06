b94674af83c24b143949569e8307e7c557051553
Nirmal Kumar Sekar <kagosekar@gmail.com>
2022-01-06 11:47:28 +0530

    Replace Spring Gateway with Zuul

--- cricketer-api-gateway/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ cricketer-api-gateway/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,9 +3,11 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 
 @EnableEurekaClient
 @SpringBootApplication
+@EnableZuulProxy
 class DemoApplication
 
     fun main(args: Array<String>) {
